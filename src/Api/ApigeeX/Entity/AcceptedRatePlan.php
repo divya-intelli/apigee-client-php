d25cb6bf6a7b5fe1a837479b56f7d7020c0d3b34
Divya Jose <divyajose@google.com>
2021-03-03 14:43:15 +0530

    Initial commit for purchased product list

---
+++ src/Api/ApigeeX/Entity/AcceptedRatePlan.php
@@ -0,0 +1,78 @@
+<?php
+
+/*
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Apigee\Edge\Api\ApigeeX\Entity;
+
+use Apigee\Edge\Api\ApigeeX\Entity\Property\EndTimePropertyAwareTrait;
+use Apigee\Edge\Api\ApigeeX\Entity\Property\StartTimePropertyAwareTrait;
+use Apigee\Edge\Api\ApigeeX\Entity\Property\ApiProductPropertyAwareTrait;
+use Apigee\Edge\Entity\Property\NamePropertyAwareTrait;
+use Apigee\Edge\Api\Monetization\Entity\Entity;
+
+abstract class AcceptedRatePlan extends Entity implements AcceptedRatePlanInterface
+{
+
+    use EndTimePropertyAwareTrait;
+    use StartTimePropertyAwareTrait;
+    use ApiProductPropertyAwareTrait;
+    use NamePropertyAwareTrait;
+
+    /** @var \DateTimeImmutable|null*/
+    protected $createdAt;
+
+    /** @var \DateTimeImmutable|null */
+    protected $lastModifiedAt;
+
+
+    /**
+     * @inheritdoc
+     */
+    public function getLastModifiedAt(): ?\DateTimeImmutable
+    {
+        return $this->$lastModifiedAt;
+    }
+
+    /**
+     * @param \DateTimeImmutable $updated
+     *
+     * @internal
+     */
+    public function setLastModifiedAt(?\DateTimeImmutable $lastModifiedAt): void
+    {
+        $this->lastModifiedAt = $lastModifiedAt;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->$createdAt;
+    }
+
+    /**
+     * @param \DateTimeImmutable|null $createdAt
+     *
+     * @internal
+     */
+    public function setCreatedAt(?\DateTimeImmutable $createdAt): void
+    {
+        $this->createdAt = $createdAt;
+    }
+
+}
